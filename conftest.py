15ed8bed63992375958e539aad23d6018523b645
Artem <twit.art@yandex.ru>
2025-11-04 14:01:34 +0300

    new changes with comments

--- conftest.py
+++ conftest.py
@@ -11,10 +11,3 @@ def create_courier():
     yield login, password, first_name
     delete_courier(login, password)
 
-@pytest.fixture
-def create_order_fixture():
-    response = order_paylord()
-    assert response.status_code == 201, "Не удалось создать заказ"
-    track = response.json()["track"]
-    yield track
-    print(f"Заказ {track} завершён")
